0f702b7dd7d6a32dc51e5448649224788136d488
GUILHERME FARIA <guilherme.ofaria@outlook.com>
2023-06-12 22:47:52 -0300

    Design Changes

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Testando o Flutter',
       theme: ThemeData(
-        // This is the theme of your application.
+        // This is the theme of your application.0
         //
         // Try running your application with "flutter run". You'll see the
         // application has a blue toolbar. Then, without quitting the app, try
@@ -22,9 +22,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.yellow,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Estudando o Flutter'),
     );
   }
 }
